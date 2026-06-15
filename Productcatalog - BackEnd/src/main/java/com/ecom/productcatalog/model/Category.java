53764bf4b253d25597d228f8626040e0d6e55def
Your Name <your_email@gmail.com>
2026-06-15 22:22:52 +0530

    Initial commit

---
+++ Productcatalog - BackEnd/src/main/java/com/ecom/productcatalog/model/Category.java
@@ -0,0 +1,44 @@
+package com.ecom.productcatalog.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category",
+                cascade = CascadeType.ALL,
+                fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
